8b49cc7d922672feec2866aa90e91996a24005dc
neelsrv <54625303+neelsrv@users.noreply.github.com>
2019-08-29 02:47:39 +0530

    Update server.php

--- server.php
+++ server.php
@@ -35,7 +35,7 @@ if (isset($_POST['reg_user'])) {
 
 		$_SESSION['username'] = $username;
 		$_SESSION['success'] = "You are now logged in";
-		header('location: index.php');
+		header('location: index.html');
 	}
 
 }
@@ -59,7 +59,7 @@ if (isset($_POST['login_user'])) {
 		if (mysqli_num_rows($results) == 1) {
 			$_SESSION['username'] = $username;
 			$_SESSION['success'] = "You are now logged in";
-			header('location: index.php');
+			header('location: index.html');
 		}else {
 			array_push($errors, "Wrong username/password combination");
 		}
