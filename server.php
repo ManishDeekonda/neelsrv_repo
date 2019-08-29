b2dbb64d660f368b497885850c22573b65149663
neelsrv <54625303+neelsrv@users.noreply.github.com>
2019-08-30 01:02:24 +0530

    Update server.php

--- server.php
+++ server.php
@@ -7,7 +7,7 @@ $errors = array();
 $_SESSION['success'] = "";
 
 // connect to database
-$db = mysqli_connect('reg.cembe82h2thk.ap-south-1.rds.amazonaws.com:3306', 'admin', 'YAMAHA1860', 'reg');
+$db = mysqli_connect('database-1.cembe82h2thk.ap-south-1.rds.amazonaws.com:3306', 'admin', 'YAMAHA1860', 'database-1');
 
 // REGISTER USER
 if (isset($_POST['reg_user'])) {
