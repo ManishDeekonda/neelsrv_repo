494dfc514617a764e03891db4656d032adfa6380
neelsrv <54625303+neelsrv@users.noreply.github.com>
2019-08-31 14:53:42 +0530

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Registration system PHP and MySQL</title>
+  <title>Registra</title>
   <link rel="stylesheet" type="text/css" href="stylesheet.css">
 </head>
 <body>
